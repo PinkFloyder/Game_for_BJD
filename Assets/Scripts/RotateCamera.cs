5c80e37e3bdc3b5720e3ee4991e1f1a4da8adc4c
Koval Leonid <lenyasweet@yandex.ru>
2021-02-25 18:50:17 +0300

    First Commit

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform _transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
