1017bf792bcd2b19e8ed39bd815e6e6018af7f06
Koval Leonid <lenyasweet@yandex.ru>
2021-03-15 13:04:44 +0300

    Help Andrey

---
+++ Assets/Scripts/wood_view.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class wood_view : MonoBehaviour
+{
+    public float step;
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    private float _progres;
+    
+    public volatile GameObject wood;
+    public volatile bool check = false;
+    
+
+    private void OnMouseDown()
+    {
+        check = !check;
+    }
+
+    private void FixedUpdate()
+    {
+        if (check) { 
+            wood.transform.position = Vector3.Lerp(startPosition, endPosition, _progres);
+            _progres += step;
+        } 
+        else { 
+            wood.transform.position = Vector3.Lerp(startPosition, endPosition, _progres);
+            _progres -= step;
+        }
+    }
+}
